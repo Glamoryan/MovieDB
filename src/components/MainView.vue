9cd6b0fd1acc1476a9acb0cd445087460ed239a7
Glamoryan <adnan.ozdemir@useinsider.com>
2024-03-14 01:35:18 +0300

    recommender

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavItem from './items/NavItem.vue'
 import BannerItem from './items/BannerItem.vue'
+import RecommenderItem from './items/RecommenderItem.vue'
 </script>
 
 <template>
   <NavItem />
   <BannerItem />
+  <RecommenderItem />
 </template>
---
+++ src/components/items/RecommenderItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommender-wrapper">
+    <div class="header">
+      <div class="title">What's Popular</div>
+      <div class="selectors">
+        <div class="selector current">Popular</div>
+        <div class="selector">Top Rated</div>
+        <div class="selector">TV Shows</div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="item-card">
+        <img class="image" src="../../assets/images/itemCard.jpg" alt="item-image" />
+        <div class="rating">85</div>
+        <div class="title">Terminator 2</div>
+        <div class="date">Jul 20, 2001</div>
+      </div>
+      <div class="item-card">
+        <img class="image" src="../../assets/images/itemCard.jpg" alt="item-image" />
+        <div class="rating">85</div>
+        <div class="title">Terminator 2</div>
+        <div class="date">Jul 20, 2001</div>
+      </div>
+      <div class="item-card">
+        <img class="image" src="../../assets/images/itemCard.jpg" alt="item-image" />
+        <div class="rating">85</div>
+        <div class="title">Terminator 2</div>
+        <div class="date">Jul 20, 2001</div>
+      </div>
+      <div class="item-card">
+        <img class="image" src="../../assets/images/itemCard.jpg" alt="item-image" />
+        <div class="rating">85</div>
+        <div class="title">Terminator 2</div>
+        <div class="date">Jul 20, 2001</div>
+      </div>
+      <div class="item-card">
+        <img class="image" src="../../assets/images/itemCard.jpg" alt="item-image" />
+        <div class="rating">85</div>
+        <div class="title">Terminator 2</div>
+        <div class="date">Jul 20, 2001</div>
+      </div>
+    </div>
+  </div>
+</template>
